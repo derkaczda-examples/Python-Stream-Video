033260e06b35b88040dffa1068c8f1a69dff388a
Daniel <daniel.c.derkacz@t-online.de>
2018-11-24 14:24:15 +0100

    added client and server file

---
+++ client.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct ### new code
+
+socket_ip = ''
+cap=cv2.VideoCapture(0)
+clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+clientsocket.connect((socket_ip,8089))
+while True:
+    ret,frame=cap.read()
+    data = pickle.dumps(frame) ### new code
+    clientsocket.sendall(struct.pack("H", len(data))+data) ### new 
