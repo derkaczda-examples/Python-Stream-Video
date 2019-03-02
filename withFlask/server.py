5198aec30e9a7aee39aa5b83d42fbcb305c30f7b
Daniel <daniel.c.derkacz@t-online.de>
2019-03-02 17:50:47 +0100

    bugfix in server.py

--- withFlask/server.py
+++ withFlask/server.py
@@ -69,5 +69,6 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
     videoCam = gen(VideoCamera())
+    app.run(host='0.0.0.0', debug=True)
+    
